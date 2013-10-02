c09afe97d38c2952efc824d55dad2d2258bb47f0
shaquiesha <sdavis@chicagotechacademy.org>
2013-10-02 13:13:01 -0500

    Thingy 2

--- challenge/6_string.rb
+++ challenge/6_string.rb
@@ -8,4 +8,11 @@
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  to_return = ""
+  string.size.times do |index|
+    if (return_odds && index.odd?) || (!return_odds && index.even?)
+      to_return << string[index]
+    end
+  end
+  to_return
 end
